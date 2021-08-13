f5da9a8ccb935f8a36533a2e3f471c2dcdc44227
Abel Souza <abelsouzacosta@gmail.com>
2021-08-13 10:53:08 -0300

    feat(errors): tratando erros assíncronos da aplicação

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,11 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import 'dotenv/config';
 import router from './routes';
 import '@shared/typeorm';
+import { ApplicationError } from '@shared/errors/ApplicationError';
 
 const app = express();
 
@@ -12,5 +13,23 @@ app.use(express.json());
 app.use(cors());
 
 app.use(router);
+
+// middleware de tratamento de erros assíncronos
+app.use(
+  // eslint-disable-next-line
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof ApplicationError) {
+      return response.status(error.statusCode).json({
+        status: 'Application Errror',
+        message: error.message,
+      });
+    }
+
+    return response.status(400).json({
+      status: 'error',
+      message: error.message,
+    });
+  },
+);
 
 app.listen(process.env.PORT);
