8a713e464d84db7d8cfcd75edf712e9b5e4e1e4b
Abel Souza <abelsouzacosta@gmail.com>
2021-08-14 12:12:30 -0300

    chore(database): adicionando migration da tabela de empresas
    Adicionando a migration da tabela de empresas

---
+++ src/shared/typeorm/migrations/1628951560702-CreateCompanies.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCompanies1628951560702 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'companies',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'corporate_name',
+            type: 'varchar',
+          },
+          {
+            name: 'trade_name',
+            type: 'varchar',
+          },
+          {
+            name: 'registration_status',
+            type: 'varchar',
+          },
+          {
+            name: 'economic_activity_type',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('companies');
+  }
+}
