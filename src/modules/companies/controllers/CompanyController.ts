9ebe7e03295219756be0a83fe4b66ddac7feb131
Abel Souza <abelsouzacosta@gmail.com>
2021-08-14 22:01:06 -0300

    feat(companies): listando as empresas
    Permite listar todas as empresas cadastradas

--- src/modules/companies/controllers/CompanyController.ts
+++ src/modules/companies/controllers/CompanyController.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express';
 import CreateCompanyService from '../services/CreateCompanyService';
+import ListCompaniesService from '../services/ListCompaniesService';
 
 export default class CompanyController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const list = new ListCompaniesService();
+
+    const companies = await list.execute();
+
+    return response.status(200).json(companies);
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
     const { cnpj } = request.body;
 
--- src/modules/companies/routes/company.routes.ts
+++ src/modules/companies/routes/company.routes.ts
@@ -4,6 +4,8 @@ import CompanyController from '../controllers/CompanyController';
 const router = Router();
 const controller = new CompanyController();
 
+router.get('/', controller.index);
+
 router.post('/', controller.create);
 
 export default router;
